9c99df313c3be3109fde144f315deab3e89d3da0
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-04-14 11:56:14 +0200

    działające podstawy symulatora

--- lista3/static/csma-web.js
+++ lista3/static/csma-web.js
@@ -23,6 +23,7 @@ CSMA.prototype.addNode = function (id, position, probability = 0.05) {
     }
     );
     if (this.verbose) console.log(`Node ${id} is added at ${position}`);
+    return this.nodes[this.nodes.length - 1];
 }
 
 CSMA.prototype.step = function () {
@@ -59,7 +60,7 @@ CSMA.prototype.step = function () {
 
     for (let n of this.nodes) {
         if (!n.emit && n.timeout === 0) {
-            if (this.line[n.position] > 0) {
+            if (this.line[n.position].length > 0) {
                 n.timeout += Math.floor(this.line.length/2);
                 if (this.verbose) console.log(`${n.id} waits for line to be clear`);
             } else {
@@ -84,7 +85,9 @@ CSMA.prototype.step = function () {
             n.emit = false;
 
             if (n.collision) {
-                n.timeout = this.line.length * Math.pow(2, n.mult);
+                let timeouts = [];
+                for (let i = 0; i <= n.mult; i++) timeouts.push(Math.pow(2, i));
+                n.timeout = this.line.length * timeouts[Math.floor(Math.random()*timeouts.length)];
                 n.mult++;
                 n.collision = false;
                 if (this.verbose) console.log(`${n.id} waits ${n.timeout}`);
@@ -110,7 +113,7 @@ function initCsma(element, csma) {
     let tab = document.createElement('table');
     let line = document.createElement('tr');
     let nodes = document.createElement('tr');
-    for (let [i, e] of csma.line.entries()) {
+    for (let e of csma.line.entries()) {
         let lineCell = document.createElement('td');
         let nodeCell = document.createElement('td');
 
@@ -124,6 +127,8 @@ function initCsma(element, csma) {
         nodeCell.appendChild(input);
         nodeCell.appendChild(button);
 
+        button.addEventListener('click', addNode(csma, tab));
+
         line.appendChild(lineCell);
         nodes.appendChild(nodeCell);
     }
@@ -133,5 +138,44 @@ function initCsma(element, csma) {
 
     tab.id = 'csma';
 
+    let stepButton = document.createElement('input');
+    stepButton.type = 'button';
+    stepButton.value = 'step';
+
+    stepButton.addEventListener('click', nextStep(csma, tab));
+
     element.appendChild(tab);
+    element.appendChild(stepButton);
+}
+
+function addNode(csma) {
+    return function(event) {
+        let id = event.target.parentNode.children[0].value;
+        let position = event.target.parentNode.cellIndex;
+
+        if (id !== '') {
+            let added = csma.addNode(id, position);
+
+            let parent = event.target.parentNode;
+            while (parent.firstChild) parent.removeChild(parent.firstChild);
+    
+            parent.textContent = `${id}\r\n${added.timeout}`;
+        }
+    }
+}
+
+function nextStep(csma, tab) {
+    return function() {
+        csma.step();
+        for (let [i, l] of csma.line.entries()) {
+            tab.children[0].children[i].textContent = '';
+            for (let e of l) {
+                tab.children[0].children[i].textContent += `${e.id}\r\n`;
+            }
+        }
+        
+        for (let n of csma.nodes) {
+            tab.children[1].children[n.position].textContent = `${n.id}\r\n${n.timeout}`;
+        }
+    }
 }
