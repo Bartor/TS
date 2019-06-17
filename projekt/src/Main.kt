7b60a690d2072ae528f2f2bfb2839bef60ff8c9c
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-06-17 14:04:59 +0200

    inne próby paraleizacji

--- projekt/src/Main.kt
+++ projekt/src/Main.kt
@@ -1,68 +1,75 @@
 import com.google.gson.Gson
 import graph.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.io.File
 import java.util.*
-import java.util.concurrent.CountDownLatch
 
 const val TRIES = 100 //how many simulations are held
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
+    val jobs = mutableListOf<Job>()
     val finalStats = arrayOfNulls<Array<Array<StatyStats?>?>?>(10)
     for (size in 10..100 step 10) {
-        finalStats[size/10 - 1] = arrayOfNulls(10)
+        finalStats[size / 10 - 1] = arrayOfNulls(10)
         for (emitters in 1..10) {
-            finalStats[size/10 - 1]!![emitters - 1] = arrayOfNulls(10)
+            finalStats[size / 10 - 1]!![emitters - 1] = arrayOfNulls(10)
             for (probability in 1..10) {
-                print("SIZE: $size EMITTERS: $emitters PROBABILITY: ${probability/100.0}")
+                jobs.add(launch(Dispatchers.Default) {
+                    print("SIZE: $size EMITTERS: $emitters PROBABILITY: ${probability / 100.0}")
 
-                val latch = CountDownLatch(4)
-
-                var circularFullStats = Stats()
-                Thread {
-                    circularFullStats = launchThreaded(TRIES) {
-                        getStats(CircularGraph(size, emitters, probability/100.0, 1.0), size*size)
+                    val circularFullStats = Stats()
+                    for (i in 0..TRIES) {
+                        val s = getStats(CircularGraph(size, emitters, probability / 100.0, 1.0), size * size)
+                        circularFullStats.collisions += s.collisions / TRIES
+                        circularFullStats.succeses += s.succeses / TRIES
+                        circularFullStats.tries += s.tries / TRIES
+                        circularFullStats.waits += s.waits / TRIES
                     }
                     print(".")
-                }.start()
 
-                var circularHalfStats = Stats()
-                Thread {
-                    circularHalfStats = launchThreaded(TRIES) {
-                        getStats(CircularGraph(size, emitters, probability/100.0, 0.5), size*size)
+
+                    val circularHalfStats = Stats()
+                    for (i in 0..TRIES) {
+                        val s = getStats(CircularGraph(size, emitters, probability / 100.0, 0.5), size * size)
+                        circularHalfStats.collisions += s.collisions / TRIES
+                        circularHalfStats.succeses += s.succeses / TRIES
+                        circularHalfStats.tries += s.tries / TRIES
+                        circularHalfStats.waits += s.waits / TRIES
                     }
-                    latch.countDown()
                     print(".")
-                }.start()
 
-                var circularQuarterStats = Stats()
-                Thread {
-                    circularQuarterStats = launchThreaded(TRIES) {
-                        getStats(CircularGraph(size, emitters, probability/100.0, 0.25), size*size)
+                    val circularQuarterStats = Stats()
+                    for (i in 0..TRIES) {
+                        val s = getStats(CircularGraph(size, emitters, probability / 100.0, 0.25), size * size)
+                        circularQuarterStats.collisions += s.collisions / TRIES
+                        circularQuarterStats.succeses += s.succeses / TRIES
+                        circularQuarterStats.tries += s.tries / TRIES
+                        circularQuarterStats.waits += s.waits / TRIES
                     }
-                    latch.countDown()
                     print(".")
-                }.start()
 
-                var starStats = Stats()
-                Thread {
-                    starStats = launchThreaded(TRIES) {
-                        getStats(StarGraph(size, emitters, probability/100.0), emitters*emitters)
+                    val starStats = Stats()
+                    for (i in 0..TRIES) {
+                        val s = getStats(StarGraph(size, emitters, probability / 100.0), emitters * emitters)
+                        starStats.collisions += s.collisions / TRIES
+                        starStats.succeses += s.succeses / TRIES
+                        starStats.tries += s.tries / TRIES
+                        starStats.waits += s.waits / TRIES
                     }
-                    latch.countDown()
                     print(".")
-                }.start()
 
-                latch.await()
 
-                finalStats[size/10 - 1]!![emitters - 1]!![probability - 1] = StatyStats(circularFullStats, circularHalfStats, circularQuarterStats, starStats)
-                println("\tdone")
+                    synchronized(finalStats) {
+                        finalStats[size / 10 - 1]!![emitters - 1]!![probability - 1] =
+                                StatyStats(circularFullStats, circularHalfStats, circularQuarterStats, starStats)
+                    }
+                    println("\tdone")
+                })
             }
         }
     }
+
+    for (job in jobs) job.join()
 
     File("$TRIES-${Calendar.getInstance().timeInMillis}.json").writeText(Gson().toJson(finalStats))
 }
@@ -72,26 +79,4 @@ fun getStats(g: AbstractGraph, steps: Int): Stats {
         g.step()
     }
     return g.stats
-}
-
-fun launchThreaded(tries: Int, what: () -> Stats) : Stats {
-    val localStats = mutableListOf<Stats>()
-    runBlocking {
-        for (i in 0 until tries) {
-            launch {
-                val s = what()
-                synchronized(localStats) {
-                    localStats.add(s)
-                }
-            }
-        }
-    }
-    val res = Stats()
-    for (s in localStats) {
-        res.collisions += s.collisions/TRIES
-        res.succeses += s.succeses/TRIES
-        res.tries += s.tries/TRIES
-        res.waits += s.waits/TRIES
-    }
-    return res
 }
